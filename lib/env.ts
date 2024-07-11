377f4edcc7cbaf54b96de575897552ca56652fd5
Elysee Bleu <elyseebleu1@gmail.com>
2024-07-11 21:36:43 +0000

    make APP NAME env optional

--- lib/env.ts
+++ lib/env.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 export const Env = createEnv({
   server: {},
   client: {
-    NEXT_PUBLIC_APP_NAME: z.string().min(1),
+    NEXT_PUBLIC_APP_NAME: z.string().optional(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
